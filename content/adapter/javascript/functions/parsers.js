1bac479643c5249c1260bf60c4844aaee41665b8
Di Coding <dicoding@yandex.ru>
2018-04-30 21:59:47 +0300

    Adapter: fix README.md and parsers.js

--- content/adapter/javascript/functions/parsers.js
+++ content/adapter/javascript/functions/parsers.js
@@ -18,10 +18,10 @@ const parsers = {
   },
 };
 
-export default format => (data) => {
-  const parse = parsers[format];
-  if (!parse) {
+export default format => {
+  const parser = parsers[format];
+  if (!parser) {
     throw new Error(`unkown format: ${format}`);
   }
-  return parse(data);
+  return parser;
 };
